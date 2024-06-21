1b0a1bfc3c536dd1c32b79c6b4c1494b25e992cf
tlannigan <tristanlannigan@gmail.com>
2024-06-21 16:13:46 -0600

    Enforce mixin name uniqueness on getMixinApplyFailures preprocessor

--- src/rules/rulePreprocessor.tsx
+++ src/rules/rulePreprocessor.tsx
@@ -29,13 +29,11 @@ export const getMissingOrUnsupportedDependencies = (lines: string[]): ReactEleme
 }
 
 export const getDuplicateMods = (lines: string[]): ReactElement[] => {
-    let alertElements = [<p key={-1}>Remove one of the following mods:</p>]
+    const alertElements = [<p key={-1}>Remove one of the following mods:</p>]
     for (let i = 1; i < lines.length; i++) {
         if (lines[i].trim().toLowerCase().startsWith('mod id: ')) {
             const indexOfFileNames = lines[i].indexOf('from mod files:') + 16
-            console.log(indexOfFileNames)
             const fileNames = lines[i].slice(indexOfFileNames).split(', ')
-            console.log(fileNames)
             for (const fileName of fileNames) {
                 alertElements.push(<p key={i}>&bull; {fileName}</p>)
             }
@@ -50,13 +48,14 @@ export const getDuplicateMods = (lines: string[]): ReactElement[] => {
 // [29May2024 12:51:10.411] [main/FATAL] [mixin/]: Mixin apply failed shulkerboxtooltip-common.mixins.json:client.ScreenMixin -> net.minecraft.client.gui.screens.Screen: org.spongepowered.asm.mixin.injection.throwables.InvalidInjectionException @ModifyArg annotation on updateTooltipLeftAndBottomPos specifies a target class 'net/minecraft/client/gui/screen/Screen', which is not supported [PREINJECT Applicator Phase -> shulkerboxtooltip-common.mixins.json:client.ScreenMixin -> Prepare Injections ->  -> modify$bng000$updateTooltipLeftAndBottomPos(Lcom/mojang/math/Matrix4f;Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIII)I -> Parse]
 // Caused by: org.spongepowered.asm.mixin.injection.throwables.InjectionError: Critical injection failure: Variable modifier method setBlockStateInjectGenerateSkylightMapVanilla(Z)Z in mixins.phosphor.json:common.MixinChunk$Vanilla from mod unknown-owner failed injection check, (0/1) succeeded. Scanned 1 target(s). Using refmap mixins.phosphor.refmap.json
 export const getMixinApplyFailures = (lines: string[]): ReactElement[] => {
-    let alertElements = [<p key={-1}>These mixins are failing to apply, try removing the mods that own them:</p>]
-    let mixinJsons = new Set(lines[0].match(/(((\w+)|(\w+(-|\.|_)\w+))\.(mixins|mixin|refmap)+.json)/g))
+    const alertElements = [<p key={-1}>These mixins are failing to apply, try removing the mod(s) that own them:</p>]
+    const mixinJsons = lines[0].match(/(((\w+)|(\w+(-|\.|_)\w+))\.(mixins|mixin|refmap)+.json)/g)
     if (mixinJsons) {
-        for (const [index, mixin] of mixinJsons.entries()) {
-            // Remove "mixins", "json", and/or "refmap" portions of the mixin name, leaving the name given by the mod author
-            const mixinName = mixin.replace(/(mixins|mixin|refmap|json)/g, '')
-            alertElements.push(<p key={index}>&bull; {mixinName.split('.')}</p>)
+        // Remove "mixins", "json", and/or "refmap" portions of the mixin name, leaving the name given by the mod author
+        const filteredMixinNames = new Set(mixinJsons.map((mixin) => mixin.replace(/(mixins|mixin|refmap|json)/g, '')))
+
+        for (const [index, mixin] of filteredMixinNames.entries()) {
+            alertElements.push(<p key={index}>&bull; {mixin.split('.')}</p>)
         }
     }
 
